541c3f0ecd41079092b5ca04f65ae4236014fece
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2022-11-04 19:16:09 -0300

    ADD: ParticlesJS, CSS Tips

---
+++ docs/assets/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  particlesJS.load("particles-js", "assets/particles.json", function () {
+    console.log("callback - particles.js config loaded");
+  });
+});
